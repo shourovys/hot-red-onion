050143a104018c32708cc52521a70e5d9a0fb493
shourov <shourovys@gmail.com>
2020-11-05 16:39:09 +0600

    Search Components done withOut functionaty

--- src/Components/HomeContainer/Home/Home.js
+++ src/Components/HomeContainer/Home/Home.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import Search from '../Search/Search';
 import './Home.css'
 const Home = () => {
     return (
         <div>
-
+            <Search />
         </div>
     );
 };
